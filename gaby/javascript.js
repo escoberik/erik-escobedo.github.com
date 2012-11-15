d88c1a83d061421a43568f5a0ebec9b8e89b7360
Erik Escobedo <erik@mojotech.com>
2012-11-15 01:15:16 -0600

    movement

--- gaby/javascript.js
+++ gaby/javascript.js
@@ -9,16 +9,40 @@ $(document).ready(function() {
     return {
       sec: sec % 60,
       min: min % 60,
-      hour: hour
+      hour: hour,
+      total: sec
     };
   };
+  window.initial_time = remainingTime();
 
   var setClock = function(time) {
     $('#clock').text(time.hour + ':' + time.min + ':' + time.sec);
-  }
-
+  };
   setClock(remainingTime());
+  
+  var setNovios = function(time) {
+    var origin = window.initial_time.total;
+    var remaining = origin - time.total;
+    var stepx = 400 / origin;
+    var stepy = 350 / 60;
+
+    var x = remaining * stepx;
+    var y = (remaining % 120) - 60;
+    console.log(y);
+
+    $('#erik').css({
+      left: x,
+      top: y * stepy + 350
+    });
+
+    $('#gaby').css({
+      right: x,
+      bottom: y * stepy + 350
+    });
+  };
+  
   setInterval(function() {
     setClock(remainingTime());
+    setNovios(remainingTime());
   }, 500);
 });
