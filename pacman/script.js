3b690291762ca903e7826f708cb4e3fd2295b47c
Erik Escobedo <erik@mojotech.com>
2014-02-07 15:10:55 -0600

    Movimiento

--- pacman/script.js
+++ pacman/script.js
@@ -16,11 +16,24 @@ $(function() {
     ]
   };
 
+  var lucero = {
+    $el: $('#lucero'),
+    x: 4,
+    y: 9,
+    speed: 4
+  };
+
   $(map.tiles).each(function(y, row) {
     $(row).each(function(x, tile) {
       var $tile = generateTile(x, y, tile);
       map.$el.append($tile);
     });
+  });
+
+  map.$el.on('click', '.tile', function() {
+    var x = $(this).data('x');
+    var y = $(this).data('y');
+    moveSprite(lucero, x, y);
   });
 });
 
@@ -28,8 +41,18 @@ var tile_width = 100;
 var tile_height = 100;
 
 function generateTile(x, y, tile) {
-  return $('<div />').attr({ class: 'tile' }).css({
+  return $('<div />').attr({ class: 'tile' }).data({
+    x: x,
+    y: y
+  }).css({
     top: y * tile_width,
     left: x * tile_height
   });
+};
+
+function moveSprite(sprite, x, y) {
+  sprite.$el.animate({
+    top: y * tile_width,
+    left: x * tile_height
+  })
 };
