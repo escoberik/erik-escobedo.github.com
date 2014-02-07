216f790e9406ec92d6abbc1d5c15c334be22a444
Erik Escobedo <erik@mojotech.com>
2014-02-07 14:56:41 -0600

    map with tiles

--- pacman/script.js
+++ pacman/script.js
@@ -1,6 +1,35 @@
 $(function() {
-  var width  = $('#map').width();
-  var height = $('#map').height();
+  var map = {
+    $el: $('#map'),
+    tiles: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+  };
 
-  alert(width + ', ' + height);
+  $(map.tiles).each(function(y, row) {
+    $(row).each(function(x, tile) {
+      var $tile = generateTile(x, y, tile);
+      map.$el.append($tile);
+    });
+  });
 });
+
+var tile_width = 100;
+var tile_height = 100;
+
+function generateTile(x, y, tile) {
+  return $('<div />').attr({ class: 'tile' }).css({
+    top: y * tile_width,
+    left: x * tile_height
+  });
+};
